d6fe877758396430699c7ef463fb54a48a13cc9d
Kim Nhật Anh <thanhchem.pro@gmail.com>
2025-03-09 23:47:55 +0700

    add skip function

---
+++ src/commands/player-commands/skip.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { useQueue } = require('discord-player');
+
+module.exports = {
+	isPlayer: true,
+	// Define the play command
+	data: new SlashCommandBuilder()
+		.setName('skip') // Command name
+		.setDescription('Skip the current song'),
+
+	execute(interaction) {
+		// Get the queue
+        const queue = useQueue();
+
+		if (!queue) {
+			const embed = new EmbedBuilder().setColor(0xFF0000).setTitle('Queue Info').setDescription('The server does not have active player!');
+			return interaction.reply({ embeds: [embed] });
+		}
+
+        if (!queue.isPlaying()) {
+			const embed = new EmbedBuilder().setColor(0xFF0000).setTitle('Queue Info').setDescription('There is no playing track!');
+			return interaction.reply({ embeds: [embed] });
+		}
+
+		// skip the track
+        const currentTrack = useQueue().currentTrack;
+        queue.node.skip();
+
+		// send the message
+		const embed = new EmbedBuilder()
+			.setColor(0xFF0000)
+			.setTitle('Queue Info')
+			.setThumbnail(currentTrack.thumbnail)
+			.addFields(
+				{ name: '⏯ Skipping: ', value: currentTrack.title, inline: true },
+			);
+
+		interaction.reply({ embeds: [embed] });
+	},
+};
