2541a84d2e28ae3076a94f8eca9e136004ae800a
Kim Nhật Anh <thanhchem.pro@gmail.com>
2025-03-09 14:36:32 +0700

    discorbot now can use the url fromn youtube source, by using dedicated extractor by another package

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { Player } = require('discord-player');
 const { DefaultExtractors } = require('@discord-player/extractor');
+const { YoutubeiExtractor } = require('discord-player-youtubei')
 
 const register = require('./ultility/setup/deployCommands');
 const commandLoader = require('./ultility/setup/loadCommands');
@@ -28,13 +29,16 @@ client.commands = new Collection;
 // A's Structure: <key(user ID) - value(timestap when invoke the command)>
 client.cooldowns = new Collection;
 
-const player = new Player(client);
+const player = new Player(client, {
+	skipFFmpeg: false,
+});
 
 async function setUp() {
 	await register.deploy();
 	await commandLoader.load(client);
 	await eventLoader.load(client, player);
 	await player.extractors.loadMulti(DefaultExtractors);
+	await player.extractors.register(YoutubeiExtractor);
 	await client.login(token);
 }
 
