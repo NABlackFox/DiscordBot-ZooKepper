84c864b58b697018aea196ad28835644eb7d138d
Kim Nhật Anh <thanhchem.pro@gmail.com>
2025-03-09 23:11:37 +0700

    add pause function

---
+++ src/commands/player-commands/pause.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { useTimeline, useQueue } = require('discord-player');
+
+module.exports = {
+	isPlayer: true,
+	// Define the play command
+	data: new SlashCommandBuilder()
+		.setName('pause') // Command name
+		.setDescription('Pause the current song'),
+
+	execute(interaction) {
+		// Get the time line
+		const timeline = useTimeline();
+
+		if (!timeline) {
+			const embed = new EmbedBuilder().setColor(0xFF0000).setTitle('Queue Info').setDescription('The server does not have active player!');
+			return interaction.reply({ embeds: [embed] });
+		}
+
+		// state of the pause
+		isPause = timeline.paused;
+
+		// pause or resume
+		isPause ? timeline.resume() : timeline.pause();
+
+		const currentTrack = useQueue().currentTrack;
+
+		// send the message
+		const embed = new EmbedBuilder()
+			.setColor(0xFF0000)
+			.setTitle('Queue Info')
+			.setThumbnail(currentTrack.thumbnail)
+			.addFields(
+				{ name: isPause ? '❚❚ Resume: ' : '▶ Pause:', value: currentTrack.title, inline: true },
+			);
+
+		interaction.reply({ embeds: [embed] });
+	},
+};
