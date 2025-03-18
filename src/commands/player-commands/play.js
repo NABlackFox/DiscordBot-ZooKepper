c1554b0f01f80c73bbd73d02aa2c05af6253a5ee
Kim Nhật Anh <thanhchem.pro@gmail.com>
2025-03-18 19:22:50 +0700

    refactor: Change inline, and space in play and voteskip

--- src/commands/player-commands/play.js
+++ src/commands/player-commands/play.js
@@ -1,93 +1,94 @@
 const {
-  SlashCommandBuilder,
-  PermissionsBitField,
-  MessageFlags,
-} = require("discord.js");
-const { useMainPlayer } = require("discord-player");
+	SlashCommandBuilder,
+	PermissionsBitField,
+	MessageFlags,
+} = require('discord.js');
+const { useMainPlayer } = require('discord-player');
 
 module.exports = {
-  isPlayer: true,
-  // Define the play command
-  data: new SlashCommandBuilder()
-    .setName("play") // Command name
-    .setDescription("Play a song in a voice channel") // Command description
-    .addStringOption(
-      (option) =>
-        option
-          .setName("song") // Option name
-          .setDescription("The song to play") // Option description
-          .setRequired(true) // Make the option required
-    ),
+	isPlayer: true,
+	// Define the play command
+	data: new SlashCommandBuilder()
+		.setName('play') // Command name
+		.setDescription('Play a song in a voice channel') // Command description
+		.addStringOption(
+			(option) =>
+				option
+					.setName('song') // Option name
+					.setDescription('The song to play') // Option description
+					.setRequired(true), // Make the option required
+		),
 
-  async execute(interaction) {
-    // Get the player instance and song query
-    const player = useMainPlayer();
-    const query = interaction.options.getString("song", true);
+	async execute(interaction) {
+		// Get the player instance and song query
+		const player = useMainPlayer();
+		const query = interaction.options.getString('song', true);
 
-    // Get the member's id and name that played the song
-    const memberName = interaction.member.user.globalName;
-    // Get the voice channel of the user and check permissions
-    const voiceChannel = interaction.member.voice.channel;
+		// Get the member's id and name that played the song
+		const memberName = interaction.member.user.globalName;
+		// Get the voice channel of the user and check permissions
+		const voiceChannel = interaction.member.voice.channel;
 
-    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
-    if (!voiceChannel) {
-      return interaction.editReply(
-        "You need to be in a voice channel to play music!"
-      );
-    }
+		await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+		if (!voiceChannel) {
+			return interaction.editReply(
+				'You need to be in a voice channel to play music!',
+			);
+		}
 
-    if (
-      interaction.guild.members.me.voice.channel &&
+		if (
+			interaction.guild.members.me.voice.channel &&
       interaction.guild.members.me.voice.channel !== voiceChannel
-    ) {
-      return interaction.editReply(
-        "I am already playing in a different voice channel!"
-      );
-    }
+		) {
+			return interaction.editReply(
+				'I am already playing in a different voice channel!',
+			);
+		}
 
-    if (
-      !voiceChannel
-        .permissionsFor(interaction.guild.members.me)
-        .has(PermissionsBitField.Flags.Connect)
-    ) {
-      return interaction.editReply(
-        "I do not have permission to join your voice channel!"
-      );
-    }
+		if (
+			!voiceChannel
+				.permissionsFor(interaction.guild.members.me)
+				.has(PermissionsBitField.Flags.Connect)
+		) {
+			return interaction.editReply(
+				'I do not have permission to join your voice channel!',
+			);
+		}
 
-    if (
-      !voiceChannel
-        .permissionsFor(interaction.guild.members.me)
-        .has(PermissionsBitField.Flags.Speak)
-    ) {
-      return interaction.editReply(
-        "I do not have permission to speak in your voice channel!"
-      );
-    }
+		if (
+			!voiceChannel
+				.permissionsFor(interaction.guild.members.me)
+				.has(PermissionsBitField.Flags.Speak)
+		) {
+			return interaction.editReply(
+				'I do not have permission to speak in your voice channel!',
+			);
+		}
 
-    try {
-      // Play the song in the voice channel
-      const result = await player.play(voiceChannel, query, {
-        nodeOptions: {
-          metadata: {
-            channel: interaction.channel,
-            memberName: memberName,
-          }, // Store text channel as metadata on the queue
-          finishEvent: true, // indicator that allow finish event to emit
-          leaveOnEmpty: false,
-          leaveOnEnd: false,
-          leaveOnStop: false,
-        },
-      });
+		try {
+			// Play the song in the voice channel
+			const result = await player.play(voiceChannel, query, {
+				nodeOptions: {
+					metadata: {
+						channel: interaction.channel,
+						memberName: memberName,
+					}, // Store text channel as metadata on the queue
+					finishEvent: true, // indicator that allow finish event to emit
+					leaveOnEmpty: false,
+					leaveOnEnd: false,
+					leaveOnStop: false,
+				},
+			});
 
-      // Reply to the user that the song has been added to the queue
-      return interaction.editReply(
-        `${result.track.title} has been added to the queue!`
-      );
-    } catch (error) {
-      // Handle any errors that occur
-      console.error(error);
-      return interaction.editReply("An error occurred while playing the song!");
-    }
-  },
+			// Reply to the user that the song has been added to the queue
+			return interaction.editReply(
+				`${result.track.title} has been added to the queue!`,
+			);
+		}
+		catch (error) {
+			// Handle any errors that occur
+			console.error(error);
+			return interaction.editReply('An error occurred while playing the song!');
+		}
+	},
 };
